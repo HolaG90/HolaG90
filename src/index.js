b1e98ad1f3ae7625e7a49ab03f35cbeb0c93632b
Hola <olayinka.giwa@rockets.utoledo.edu>
2022-03-02 14:17:13 -0500

    lesson22

--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
 import react from 'react'
 import reactDom from 'react-dom'
-
-
+// importing CSS file . 
+// if the file is a level up then use '../'     depending on how many levels means u have ///to add the dots 
+import './index.css';
 function Greeting() 
 {
   return (
-    <section>
+    <section className='pageDsign'>
+      <HomePg />
+      <HomePg />
+      <HomePg />
+      <HomePg />
+      <HomePg />
       <HomePg />
     </section>
   );
 }
 const HomePg =  () =>{
   return (
-    <article>
+    <article className='chargeShow'>
       <Image></Image>
       <Author></Author>
       <Title></Title>
@@ -20,7 +26,7 @@ const HomePg =  () =>{
   )
 }
 
-const Image = () => <img src="https://i0.wp.com/electrek.co/wp-content/uploads/sites/3/2020/05/Tesla-charging.jpg?w=2500&quality=82&strip=all&ssl=1" alt="" />
+const Image = () => <img src="https://cdn.jdpower.com/JDP_Tesla%20Home%20Charging%201.jpg" alt="" />
 const Author = () => <h1> Olayinka Giwa</h1>
 const Title = () => <h3>Home Charging Stations</h3>
 
